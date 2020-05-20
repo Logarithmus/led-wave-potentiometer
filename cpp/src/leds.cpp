f749a5254feada0bfb1d44e1a7b9250258f691b7
Logarithmus <logarithmus.dev@gmail.com>
2020-05-20 10:47:47 +0300

    Works, but contains some ugly hacks

--- cpp/src/leds.cpp
+++ cpp/src/leds.cpp
@@ -32,21 +32,30 @@ namespace {
 namespace leds {
 	void task(void*) {
 		constexpr uint8_t LEDS_COUNT = 10;
-		constexpr uint16_t WAVE_START = 1 << (LEDS_COUNT - 1);
+		//constexpr uint16_t WAVE_START = 1 << (LEDS_COUNT - 1);
 		constexpr uint16_t MIN_DELAY = 200;
 		constexpr uint16_t MAX_DELAY = 5000;
 
 		const auto adc_task = xTaskGetHandle("adc");
 		init();
-		bool direction = true;
-		uint16_t wave = WAVE_START;
+		uint8_t direction = -1;
+		uint16_t wave = 0;
+		uint8_t i = 9;
 		while (true) {
 			uint16_t speed = ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(1000));
 			uint16_t period_ms = MAX_DELAY - ((MAX_DELAY - MIN_DELAY) / UINT8_MAX) * speed;
 			uint16_t delay = pdMS_TO_TICKS(period_ms / (LEDS_COUNT * 2));
+			
+			if ((wave & 1023) == 1023) {
+				direction = 1;
+				wave ^= 1;
+			} else if (wave == 0) {
+				direction = -1;
+				// wave ^= (1 << 9);
+			}
+			wave ^= (1 << i);
+			i += direction;
 			set(wave);
-			direction ? wave >>= 1 : wave <<= 1;
-			direction ^= ((wave == 1) || (wave == WAVE_START));
 			xTaskNotifyGive(adc_task);
 			vTaskDelay(delay);
 		}
